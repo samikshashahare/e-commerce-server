bfdb73582d1675efd9b36b3ad77e6ba4b71c807f
samikshashahare <samikshashahare953@gmail.com>
2023-10-23 11:37:52 +0530

    implemented delete, put, patch methods

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mongoose ,{model, Schema}from 'mongoose';
+import mongoose, { model, Schema } from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,27 +9,27 @@ app.use(express.json());
 
 const PORT = 8080;
 
-const  MONGODB_URI = '';
+const MONGODB_URI = '';
 
-const connectMongoDB = async()=>{
-   const conn = await mongoose.connect(process.env.MONGODB_URI);
-   if(conn){
-    console.log('MongoDB connected successfully');
-   }
+const connectMongoDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    if (conn) {
+        console.log('MongoDB connected successfully');
+    }
 }
 connectMongoDB();
 
 const productSchema = new Schema({
-    name : String,
-    description : String,
+    name: String,
+    description: String,
     price: Number,
-    brand:String,
+    brand: String,
     productimage: String,
 });
 
 const Product = model('Product', productSchema);
 
-app.get('/products', async(req, res) => {
+app.get('/products', async (req, res) => {
     const products = await Product.find();
     res.json({
         success: true,
@@ -42,15 +42,15 @@ app.post('/products', async (req, res) => {
 
     const { name, description, price, brand, productimage } = req.body;
 
-   const newProduct = new Product({
-    name:name,
-    description:description,
-    price:price,
-    productimage:productimage,
-    brand:brand
-   })
+    const newProduct = new Product({
+        name: name,
+        description: description,
+        price: price,
+        productimage: productimage,
+        brand: brand
+    })
 
- const savedProducts = await newProduct.save();
+    const savedProducts = await newProduct.save();
 
     res.json(
         {
@@ -62,21 +62,93 @@ app.post('/products', async (req, res) => {
 
 })
 
-app.get('/product', async(req, res)=>{
-const {name} = req.query;
+app.get('/product', async (req, res) => {
+    const { name } = req.query;
 
-const findProduct = await Product.findOne({
-    name:name
-})
+    const findProduct = await Product.findOne({
+        name: name
+    })
 
-res.json({
-    success:true,
-    data:findProduct,
+    res.json({
+        success: true,
+        data: findProduct,
 
-    message : 'successfully fetched product'
-})
+        message: 'successfully fetched product'
+    })
 
 });
+
+app.delete('/product/:_id', async (req, res) => {
+    const { _id } = req.params;
+
+    await Product.deleteOne({ _id: _id });
+
+    res.json({
+        success: true,
+        data: {},
+        message: `product deleted successfully with id ${_id}`
+    })
+})
+
+app.put('/product/:_id', async (req, res) => {
+    const { _id } = req.params;
+    const { name, description, price, productimage, brand } = req.body;
+
+
+    await Product.updateOne(
+        { _id: _id },
+        {
+            $set: {
+                name: name,
+                description: description,
+                price: price,
+                productimage: productimage,
+                brand: brand
+            }
+        })
+
+    const updatedproduct = await Product.findOne({_id: _id})
+
+    res.json({
+        success:true,
+        data:updatedproduct,
+        message:'product updated successfully '
+    })
+})
+
+app.patch('/product/:_id', async(req, res)=>{
+    const {_id} = req.params;
+    const { name, description, price, productimage, brand} = req.body;
+
+    //findOne({_id: _id}) =  findById(_id) it is mongoose library feature which is little bit faster than first querry method
+    const findProduct = await Product.findById(_id);
+
+    if(name){
+        Product.name = name;
+    }
+    if(description){
+        Product.description =description;
+    }
+    if(price){
+        Product.price = price;
+    }
+    if(productimage){
+        Product.productimage= productimage;
+    }
+    if(brand){
+        Product.brand = brand;
+    }
+
+    const updatedProduct = await findProduct.save();
+
+    res.json({
+        success:true,
+        data: updatedProduct,
+        message:'successfully updated'
+    })
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
